f516b504cab918f5b5faf56ff127ef0560c99b0a
prospyb <ekuajiprosper@gmail.com>
2023-02-13 01:03:43 +0100

    answered the asssignment question

---
+++ index.js
@@ -0,0 +1,10 @@
+function multiply (value1, value2) { return value1 * value2 }
+console.log(multiply (11, 11))
+
+let firstname = 'Prosper';
+let lastname = 'Ekuaji';
+let stuterntrack = 'Backend development';
+let skills = 'HTML and CSS';
+let favouritecolor = 'Black';
+let independenceday = 'October 1st, 1990'
+console.log('My name is ' + firstname + ' ' + lastname + '. I am now enrolled in the Stutern ' + stuterntrack + ' track. I am proficient with ' + skills + ', and my favourite color is ' + favouritecolor + '. My countrys independence day is ' + independenceday + '.')
